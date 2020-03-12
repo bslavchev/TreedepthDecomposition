ae412f2f37fc04c9abaf4eff6bf2d7c69f9f0502
bslavchev <bvslavchev@gmail.com>
2020-03-12 19:18:04 +0100

    implemented Node class

--- src/solvers/mcts/core/Node.java
+++ src/solvers/mcts/core/Node.java
@@ -1,6 +1,36 @@
 package solvers.mcts.core;
 
-public class Node {
-	//TODO
+import java.util.HashSet;
+import java.util.Set;
 
+import core.DynamicComponent;
+
+public class Node {
+	private DynamicComponent state;
+	private Set<HyperEdge> inbound = new HashSet<HyperEdge>();
+	private Set<HyperEdge> outbound = new HashSet<HyperEdge>();
+	
+	private int visits = 0;
+	private double totalResult = 0;
+	
+	public Node(DynamicComponent state, HyperEdge parentEdge) {
+		this.state = state;
+		
+		addParent(parentEdge);
+	}
+	
+	public void addParent(HyperEdge parentEdge) { inbound.add(parentEdge); }
+	public Set<HyperEdge> getParents() { return inbound; }
+	
+	
+	public void addChild(HyperEdge childEdge) { outbound.add(childEdge); }
+	public Set<HyperEdge> getChildren() { return outbound; } 
+	
+	public void addVisit(double result) {
+		visits++;
+		totalResult += result;
+	}
+	
+	public int getVisits() { return visits; }
+	public double getResult() { return totalResult/visits; }
 }
